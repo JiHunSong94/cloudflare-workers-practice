3a197327a1271f10efbf85e51745c00fef7d141b
Jihun Song <songjihun940409@gmail.com>
2022-12-29 01:21:18 +0900

    1.2 Workers KV

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@
 
 export interface Env {
   // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
+  DB: KVNamespace;
   //
   // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
   // MY_DURABLE_OBJECT: DurableObjectNamespace;
@@ -29,6 +29,7 @@ export default {
   ): Promise<Response> {
     const url = new URL(request.url);
     if (url.pathname === "/") {
+      await env.DB.put("hello", "how are you?");
       return new Response(home, {
         headers: {
           "Content-Type": "text/html;chartset=utf-8",
